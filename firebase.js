52f6296e8209d10b1e46ad30cd7ef83ed204d75b
Yoko Andrae <yokoandrae@Yokos-MBP.lan>
2018-02-12 12:42:11 +0100

    initial commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyC3Z9ZIsiIdoKi-YXVniEQ5vljwqeG_MFk",
+    authDomain: "list-app-f6838.firebaseapp.com",
+    databaseURL: "https://list-app-f6838.firebaseio.com",
+    projectId: "list-app-f6838",
+    storageBucket: "",
+    messagingSenderId: "189177939471"
+  };
+  firebase.initializeApp(config);
