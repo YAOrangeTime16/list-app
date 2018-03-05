35373f61a8c54d1a2dd45e102a7d95687b6826a8
Yoko Andrae <yokoandrae@Yokos-MBP.lan>
2018-03-05 15:44:45 +0100

    added new functinality, statics, to admin page

--- src/ManageUser/Admin.js
+++ src/ManageUser/Admin.js
@@ -11,7 +11,11 @@ export default class Admin extends Component {
 	state = {
 		createGroup: false,
 		deleteConfirm: false,
-		groupToDelete: ''
+		groupToDelete: '',
+		openStatics: false,
+		statsFlip: '',
+		statsVote: '',
+		offlineMessage: ''
 	}
 
 	_deleteTheGroup = groupid => {
@@ -27,6 +31,32 @@ export default class Admin extends Component {
 
 	_openCreatePage = () => this.setState({createGroup: true})
 	_cancelCreatePage = () => this.setState({createGroup: false})
+
+	_openStatics = groupID => {
+		firebase.database().ref('.info/connected')
+		.on('value', snap =>{
+			if(snap.val()===false){
+				this.setState({offlineMessage: 'You are offline now: Information is not available when you are offline'})
+			}
+			//online
+				const groupRef = firebase.database().ref(`/groups/${groupID}`)
+				
+				groupRef.child('listFlipID').on('value', id => {
+					if(id.val() !== '' || id.val() === undefined){
+						firebase.database().ref(`/flipLists/${id.val()}`)
+						.on('value', listInfo => this.setState({statsFlip: listInfo.val()}))
+					}
+				})
+				groupRef.child('listVoteID').once('value', id => {
+					if(id.val() !== '' || id.val() === undefined){
+						firebase.database().ref(`/voteLists/${id.val()}`)
+						.on('value', listInfo => this.setState({statsVote: listInfo.val()}))
+					}
+				})
+		})
+		this.setState({openStatics: true})
+	}
+	_closeStatics = () => this.setState({openStatics: false, offlineMessage: ''})
 	
 	_renderGroupList = groupArray => {
 		const {deleteConfirm} = this.state;
@@ -34,29 +64,61 @@ export default class Admin extends Component {
 			if(groupArray){
 				return groupArray.map( group => (
 					<li key={group.groupId} className="admin-grouplist">
-						<div onClick={()=>this._deleteConfirm(group.groupId)}>
-							<DeleteIcon />
-						</div>
-						<div onClick={()=>getGroupId(group.groupId)}>
-							<Link to={{
-											pathname: '/groups/admin',
-											search: `?groupID=${group.groupId}`
-										}}
-							>{group.groupName}</Link>
-						</div>
-						<ArrowMenu />
+							<div onClick={()=>this._deleteConfirm(group.groupId)}>
+								<DeleteIcon />
+							</div>
+							<div onClick={()=>getGroupId(group.groupId)}>
+								<Link to={{
+												pathname: '/groups/admin',
+												search: `?groupID=${group.groupId}`
+											}}
+								>{group.groupName}</Link>
+							</div>
+							<ArrowMenu />
+							<div onClick={()=>this._openStatics(group.groupId)}>
+								<div className="icon-statics">Statics</div>
+							</div>
 					</li>
 					)
 				)
 			}
 	}
 
+	_renderFlipResult = items => (
+		<div>
+			{items.map( item => <li key={item.id}>{item.name}: {item.status !=='' ? 'taken' : 'not taken'}</li> )}
+		</div>
+	)
+
+	_renderVoteResult = items => (
+		<div>
+			{items.map( item => <li key={item.id}>{item.name}: {item.status ==='' ? '0' : item.status.length}</li> )}
+		</div>
+	)
+	
+
 	render(){
-		const {createGroup, deleteConfirm, groupToDelete} = this.state;
+		const {createGroup, deleteConfirm, groupToDelete, offlineMessage, statsFlip, statsVote} = this.state;
 		const {addGroup, groups, userName} = this.props;
 		return (
 			<div>
 				<h3>Logged in as: {userName}</h3>
+				{ this.state.openStatics && 
+							<div className="modal">
+								<div className="modal-innerbox-large">
+								{ offlineMessage
+									? <div>{offlineMessage}</div>
+									: (<div>
+											<div>List Title (flip): {statsFlip.label || 'no list'}</div>
+											<ul>{statsFlip.items && this._renderFlipResult(statsFlip.items)}</ul>
+											<div>List Title (vote): {statsVote.label || 'no list'}</div>
+											<ul>{statsVote.items && this._renderVoteResult(statsVote.items)}</ul>
+										</div>)
+								}
+									<Button clickAction={this._closeStatics} title="Close"/>
+								</div>
+							</div>
+				}
 				{ createGroup
 					? <CreateGroup cancelCreatePage={this._cancelCreatePage} addGroup={addGroup}/>
 					: (	
